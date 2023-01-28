b9f8b62dc4826e3887a3537c114234645b3d6c70
wseven7677 <caoyu_92@126.com>
2023-01-28 15:37:40 +0800

    feat: publish for badge update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 ## v0.1 test version
 
+### v0.1.3
+- fix badge
+
 ### v0.1.2
 - fix tomato -V version code
 
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Command } from 'commander';
 import commandStart from './commands/start.js';
 
 const program = new Command();
-program.version('0.1.2');
+program.version('0.1.3');
 program
   .command('start')
   .description('start a 25 minutes timer')
